7cd58f909df8b3c698702368061f21360a67dacb
ebbamoon <crystal9807@naver.com>
2024-10-19 14:48:38 +0900

    pycharm

--- main.py
+++ main.py
@@ -1,5 +1,6 @@
 # Ebba Moon's Streamlit Portfolio
 # Shows main projects, resume etc
 
-#
+# Ebba Moon
+# Data Scientist / AI Engineer
 # Hi! I’m a Data Scientist and AI Engineer skilled in Python, SQL, and Machine Learning.
