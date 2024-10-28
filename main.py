33206439022c73f975bf929013cb38306e08b199
ebbamoon <crystal9807@naver.com>
2024-10-29 00:24:28 +0900

    pycharm

--- main.py
+++ main.py
@@ -52,9 +52,11 @@ with st.container():
     row2_col1, row2_col2 = st.columns([1, 1])
     with row1_col1:
         st.markdown("### 1. MUSINSA (Seoul, Korea)")
-        st.markdown("<span style='font-size:18px;'>"
-                    "- Click on the section you want to expand and explore in-depth information about each tourist spot.</span>",
+        st.text('Biggest Fashion Platform Company in Korea')
+        st.markdown("<span style='font-size:18px;'>Each region features up to 5 top attractions selected through the analysis of reviews and transportation data.</span>",
                     unsafe_allow_html=True)
+
+
     with row1_col2:
         st.image("pages/data/data2.jpg", width=400, caption="MUSINSA")
 
