5fbe2c62f8e8ffbcf1dfabb4d58ac8b40a5e1640
ebbamoon <crystal9807@naver.com>
2024-10-29 00:26:26 +0900

    pycharm

--- main.py
+++ main.py
@@ -52,7 +52,7 @@ with st.container():
     row2_col1, row2_col2 = st.columns([1, 1])
     with row1_col1:
         st.markdown("### 1. MUSINSA (Seoul, Korea)")
-        st.text('From Biggest Fashion Platform Company in Korea')
+        st.text('From 2022 to 2024 / Biggest Fashion Platform Company in Korea')
         st.markdown("<span style='font-size:18px;'>- Each region features up to 5 top attractions selected through the analysis of reviews and transportation data.</span>",
                     unsafe_allow_html=True)
 
