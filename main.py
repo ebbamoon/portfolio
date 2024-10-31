57ddf88d9188c879248cd5f8d4533d6c0bd83889
ebbamoon <crystal9807@naver.com>
2024-11-01 00:39:19 +0900

    pycharm

--- main.py
+++ main.py
@@ -35,7 +35,7 @@ st.header("""Hi! I’m a Data Analyst skilled in Python, SQL, Machine Learning a
 st.write('  ')
 st.image("pages/data/data.jpg") # caption="portfolio"
 
-# 코딩 가능 Languag
+# 코딩 가능 Language
 st.divider()
 st.markdown("""
             ### 💡Programming Languages and Skills
